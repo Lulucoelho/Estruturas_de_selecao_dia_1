944391a4dd1211f751be034e429a8060f400274b
luisacoelho2005@gmail.com <luisadacruzcoelho2005@outlook.com>
2023-09-11 20:17:02 -0300

    ap03(selecao): import e estrutura

---
+++ ExemploIF.java
@@ -0,0 +1,14 @@
+//java.lang padrao
+import javax.swing.JOptionPane;
+public class ExemploIF{
+    static public  void main(String [] args){
+        //declarção de variaveis 
+        
+        //entrada
+
+        //processamento
+
+        //saida
+
+    }
+}
