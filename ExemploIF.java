4b08daf6a5a577548d3428f6f1f69e07ee1a003f
luisacoelho2005@gmail.com <luisadacruzcoelho2005@outlook.com>
2023-09-11 21:07:32 -0300

    ap03(selecao): fim do exemplo if

--- ExemploIF.java
+++ ExemploIF.java
@@ -3,12 +3,14 @@ import javax.swing.JOptionPane;
 public class ExemploIF{
     static public  void main(String [] args){
         //declarção de variaveis 
-        
         //entrada
-
+        double nota =  
+            Double.parseDouble(JOptionPane.showInputDialog
+            ("Digite a nota"));
         //processamento
-
         //saida
-
+        if (nota >= 70){
+            JOptionPane.showMessageDialog(null, "Aprovado");
+        }
     }
 }
